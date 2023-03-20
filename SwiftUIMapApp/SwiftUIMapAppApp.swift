58a2415cf40aa3845f15356dd0fefb5327d1078f
Evans Nguyen <evans.trungnguyen@setel.com>
2023-03-20 21:54:11 +0700

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Trung Nguyen on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
